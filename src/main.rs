c781f9e3a93fd7dfe247fa612ddb11991ed99340
ovlues <osvlues@gmail.com>
2024-06-23 22:57:02 +0900

    좌표 읽어오기

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io;
+
 fn main() {
+    loop {
+        let end = false;
+
+        let mut coor = String::new();
+        io::stdin().read_line(&mut coor).expect("좌표 읽기 오류");
+        let coorvec:Vec<i16> = coor.split_whitespace()
+                                    .map(|s| s.trim().parse().expect("좌표 파싱 오류"))
+                                    .collect::<Vec<_>>();
+    
+        println!("{:?}", coorvec);
+        let mut x: &i16 = &coorvec[0];
+        let mut y: &i16 = &coorvec[1];
+
+        if end {
+            break;
+        }
+    }
+
     println!("Hello, world!");
 }
